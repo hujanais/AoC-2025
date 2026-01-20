d3825274c756ca3c7a3e3a9c7a82b0df92faf28e
Teik Lee <tlee@morsecorp.com>
2026-01-20 17:22:03 -0500

    sync

--- solutions/day10.py
+++ solutions/day10.py
@@ -3,14 +3,15 @@ def day10():
     problem_sets = read_file(
         "./data/day10.txt"
     )  # 422 had to run row 66 with depth of 10.
-    # solve_a(problem_sets)
-    solve_b(problem_sets)
+
+    solve_a(problem_sets)  # 7,
+    # solve_b(problem_sets)  # 33
 
 
 def solve_a(problem_sets):
     result = 0
     idx = 0
-    for prob in problem_sets:
+    for prob in problem_sets[66:67]:
         target = prob[0]
         initial_state = [False for i in range(len(target))]
         target = prob[0]
@@ -21,11 +22,31 @@ def solve_a(problem_sets):
         print(f"{idx} min_steps = {min_steps}, path = {shortest_path}")
         result += min_steps
         idx += 1
-    print(f"result = {result}")
+    print(f"result a = {result}")
 
 
 def solve_b(problem_sets):
-    pass
+    result = 0
+    idx = 0
+    for prob in problem_sets[0:1]:
+        target = prob[0]
+        initial_state = [False for i in range(len(target))]
+        initial_joltages = [0 for i in range(len(target))]
+        target = prob[0]
+        actions = prob[1]
+        joltage_str = prob[2].replace("{", "").replace("}", "")
+        joltage_target = list(map(lambda x: int(x), joltage_str.split(",")))
+
+        min_steps = exp_b(
+            joltage_target=joltage_target,
+            actions=actions,
+            joltages=initial_joltages,
+            state=initial_state,
+        )
+        print(f"{idx} min_steps = {min_steps}")
+        result += min_steps
+        idx += 1
+    print(f"result b = {result}")
 
 
 def exp(
@@ -35,6 +56,7 @@ def exp(
     last_action_idx=-1,
     depth=0,
     path: list[list[int]] | None = None,
+    memo=None,
 ):
     if path is None:
         path = []
@@ -42,6 +64,13 @@ def exp(
     # actions  # [[3],[1,3],[2,3]]
 
     indent = "." * (depth - 1)
+
+    if memo is None:
+        memo = {}
+
+    key = tuple(state)
+    # if key in memo:
+    #     return memo[key]
 
     path_set = set(tuple(inner_list) for inner_list in path)
     if len(path_set) != len(path):
@@ -53,7 +82,7 @@ def exp(
         return (0, path.copy())
 
     # prevent infinite loop
-    if depth > 7:
+    if depth > 10:
         return (100000, None)
 
     min_steps = 100000
@@ -62,7 +91,7 @@ def exp(
     for i in range(len(actions)):
         if i == last_action_idx:
             continue
-        new_state = perform_action(state, actions[i])
+        new_state, new_joltages = perform_action(state, actions[i], joltage_input=None)
         path.append(actions[i])
 
         if len(path) > min_steps:
@@ -76,6 +105,7 @@ def exp(
                 last_action_idx=i,
                 depth=depth + 1,
                 path=path,
+                memo=memo,
             )
             steps += 1
 
@@ -85,16 +115,85 @@ def exp(
 
         path.pop()  # Backtrack: remove the action we just tried
 
+    memo[key] = (min_steps, best_path)
     return (min_steps, best_path)
 
 
-def perform_action(state: list[bool], action_arr: list[int]) -> list[bool]:
+def exp_b(
+    joltage_target: list[int],
+    actions: list[list[int]],
+    joltages: list[int],
+    state: list[bool],
+    idx: int = 0,
+    depth=0,
+    dp=None,
+):
+    # target # [False,True,True,False]
+    # actions  # [[3],[1,3],[2,3]]
+
+    indent = "." * (depth - 1)
+
+    if dp is None:
+        dp = {}
+
+    key = tuple(joltages)
+    if key in dp:
+        return dp[key]
+
+    if depth > 50:  # Adjust based on problem constraints
+        return 100000
+
+    if joltages == joltage_target:
+        print("done")
+        return 0
+
+    if any(a > b for a, b in zip(joltages, joltage_target)):
+        return 100000
+
+    print(f"{indent} {actions[idx]}")
+
+    # prevent infinite loop
+    if depth > 11:
+        return 100000
+
+    min_steps = 100000
+
+    for i in range(len(actions)):
+        new_state, new_joltages = perform_action(
+            state, actions[i], joltage_input=joltages
+        )
+
+        steps = exp_b(
+            joltage_target,
+            actions,
+            new_joltages,
+            new_state,
+            i,
+            depth=depth + 1,
+            dp=dp,
+        )
+        steps += 1
+
+        if steps < min_steps:
+            min_steps = steps
+
+    dp[key] = min_steps
+    return min_steps
+
+
+def perform_action(
+    state: list[bool], action_arr: list[int], joltage_input: list[int] = None
+) -> tuple[list[bool], list[int]]:
     # Create a copy to avoid mutating the original state
     new_state = state.copy()
+    joltages = joltage_input.copy() if joltage_input else None
+
     for action in action_arr:
         new_state[action] = not new_state[action]
+        if joltage_input:
+            joltages[action] += 1
 
-    return new_state
+    return new_state, joltages
 
 
 def read_file(filepath: str):
--- solutions/day9.py
+++ solutions/day9.py
@@ -2,12 +2,12 @@ def day9():
     solve_for_b = True
 
     # grid = read_file("./data/day9_test.txt")  # 50, 24
-    grid = read_file("./data/day9.txt")  # 4741848414
-    if solve_for_b:
-        outline = find_rect_outline(grid)
-    else:
-        outline = None
-    find_max_rectangle(grid, outline)
+    grid = read_file("./data/day9.txt")  # 4741848414, 4633585821(too high)
+    # if solve_for_b:
+    #     outline = find_rect_outline(grid)
+    # else:
+    #     outline = None
+    # find_max_rectangle(grid, outline)
 
 
 def read_file(filepath: str) -> list[list[int]]:
@@ -20,21 +20,22 @@ def read_file(filepath: str) -> list[list[int]]:
         pos = line.split(",")
         positions.append((int(pos[1]), int(pos[0])))  # convert to row,col format
 
+    # sort by rows
     positions.sort(key=lambda x: x[0])
 
-    # max_rows = max(positions, key=lambda x: x[0])[0]
-    # max_cols = max(positions, key=lambda x: x[1])[1]
+    max_rows = max(positions, key=lambda x: x[0])[0]
+    max_cols = max(positions, key=lambda x: x[1])[1]
 
-    # for position in positions:
-    #     print(position)
+    for position in positions:
+        print(position)
 
-    # for row in range(max_rows):
-    #     for col in range(max_cols):
-    #         if (row, col) in positions:
-    #             print("#", end="")
-    #         else:
-    #             print(".", end="")
-    #     print()
+    for row in range(max_rows):
+        for col in range(max_cols):
+            if (row, col) in positions:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
 
     return positions
 
@@ -64,8 +65,18 @@ def find_max_rectangle(
 ):
     rectangles: list[list[int, tuple[int, int]]] = []
     max_area = 0
+    count = 122760
+
+    # pre-sort once to optimize
+    outline_sorted_by_rows = sorted(outline, key=lambda x: x[0])
+    outline_sorted_by_cols = sorted(outline, key=lambda x: x[1])
+
     for i in range(len(positions) - 1):
         for j in range(i + 1, len(positions)):
+
+            if count % 100 == 0:
+                print(f"{count}")
+            count -= 1
             r0, c0 = positions[i]
             r1, c1 = positions[j]
             area = abs((r1 - r0 + 1) * (c1 - c0 + 1))
@@ -76,21 +87,22 @@ def find_max_rectangle(
             bl = (min(r0, r1), max(c0, c1))
             br = (max(r0, r1), max(c0, c1))
 
-            # this is for part-bn
-            print([area, (ul, ur, bl, br)])
+            # this is for part-b
             if outline:
                 if (
-                    ray_trace(ul, outline)
-                    and ray_trace(ur, outline)
-                    and ray_trace(bl, outline)
-                    and ray_trace(br, outline)
+                    ray_trace(ul, outline_sorted_by_rows, outline_sorted_by_cols)
+                    and ray_trace(ur, outline_sorted_by_rows, outline_sorted_by_cols)
+                    and ray_trace(bl, outline_sorted_by_rows, outline_sorted_by_cols)
+                    and ray_trace(br, outline_sorted_by_rows, outline_sorted_by_cols)
                 ):
-                    rectangles.append([area, (ul, ur, bl, br)])
+                    # rectangles.append([area, (ul, ur, bl, br)]) # don't do this to save memory
                     max_area = max(max_area, area)
             else:
                 rectangles.append([area, (ul, ur, bl, br)])
                 max_area = max(max_area, area)
+
     rectangles.sort(key=lambda x: x[0], reverse=True)
+
     for rect in rectangles:
         print(rect)
 
@@ -98,28 +110,29 @@ def find_max_rectangle(
 
 
 # use ray-tracing to see if the point will intersect 4 sides of the outline.
-def ray_trace(point: tuple[int, int], points: list[tuple[int, int]]) -> bool:
+def ray_trace(
+    point: tuple[int, int], outline_sorted_by_rows, outline_sorted_by_cols
+) -> bool:
     row, col = point
 
     # test row
-    points.sort(key=lambda x: x[0])
-    test_line = list(filter(lambda x: x[1] == col, points))
+    test_line = list(filter(lambda x: x[1] == col, outline_sorted_by_rows))
     row_lowerbound, _ = test_line[0]
     row_upperbound, _ = test_line[-1]
     if row < row_lowerbound or row > row_upperbound:
         return False
 
     # test column
-    points.sort(key=lambda x: x[1])
-    test_line = list(filter(lambda x: x[0] == row, points))
+    test_line = list(filter(lambda x: x[0] == row, outline_sorted_by_cols))
     _, col_lowerbound = test_line[0]
-    _,col_upperbound = test_line[-1]
+    _, col_upperbound = test_line[-1]
     # scan right
     # scan left
     if col < col_lowerbound or col > col_upperbound:
         return False
 
     return True
+
 
 if __name__ == "__main__":
     day9()
